8b81ba8e90bc1975da5a6ed954680b838632b3d0
zhangcunxin <zcunxin@163.com>
2017-03-29 15:19:37 +0800

    add some mustache demo

---
+++ src/components/Once.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p v-once>hello {{world}}</p>
+    <button @click="world='cunxin'">改变world值</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        world: 'world'
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello";
+import Once from "@/components/Once";
+import Html from "@/components/Html";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/v-once',
+      name: 'Once',
+      component: Once
+    },
+    {
+      path: '/html',
+      name: 'Html',
+      component: Html
     }
   ]
 })
