e668dd05c79e1165ac766f4976317fdc22335344
zhangcunxin <zcunxin@163.com>
2017-04-11 14:24:39 +0800

    source-map

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/,
-            loader: "style-loader!css"
+            loader: "style-loader!css-loader"
         }, {
             test: /\.js$/,
             loader: "babel-loader",
@@ -31,7 +31,7 @@ module.exports = {
             options: {
                 loaders: {
                     scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
-                    sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+                    sass: 'vue-style-loader!css-loader!sass-loader' // <style lang="sass">
                 }
             }
         }, {
@@ -42,10 +42,8 @@ module.exports = {
             loader: 'html-loader'
         }, {
             test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader?sourceMap'
+            loader: 'style-loader!css-loader!sass-loader'
         }]
-        // 开启source-map，webpack有多种source-map，在官网文档可以查到
-        // devtool: 'eval-source-map'
     },
     plugins: [
         new Webpack.LoaderOptionsPlugin({
@@ -53,7 +51,8 @@ module.exports = {
                 babel: {
                     presets: ['es2015'],
                     plugins: ['transform-runtime']
-                }
+                },
+                context: '/'
             }
         }),
         new HtmlWebpackPlugin({
@@ -67,7 +66,8 @@ module.exports = {
                 dead_code: true,
                 warnings: false
             },
-            comments: false
+            comments: false,
+            sourceMap: true
         })
 
     ],
@@ -79,5 +79,8 @@ module.exports = {
             filter: path.join(__dirname, "./src/filters"),
             components: path.join(__dirname, "./src/components")
         }
-    }
+    },
+    // 开启source-map，webpack有多种source-map，在官网文档可以查到
+    devtool: 'source-map'
+
 }
